6a5a725e28f87f25ab4713a3bb4511b884fbcf2f
Damola Oladipo <damolaoladipo@users.noreply.github.com>
2024-12-25 22:43:51 +0100

    node project setup

---
+++ src/utils/enums.util.ts
@@ -0,0 +1,19 @@
+export enum ENVType {
+    PRODUCTION = 'production',
+    STAGING = 'staging',
+    DEVELOPMENT = 'development'
+    }
+
+export enum AppChannel {
+    WEB = "web",
+    MOBILE = "mobile",
+    DESKTOP = "desktop",
+    WATCH = "watch"
+    }
+    
+export enum UserType {
+    USER = "user",
+    SUPER = "superadmin",
+    ADMIN = "admin"
+    }
+    
